47cb271c854aa2e9f9df4629e387a3a61a54b066
DESKTOP-TCSIL1V\ict03-23 <marril64@naver.com>
2022-05-27 19:40:14 +0900

    05/27

---
+++ SpringDB/src/test/java/com/ict/dao/OjdbcConnectionTest.java
@@ -0,0 +1,41 @@
+package com.ict.dao;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class OjdbcConnectionTest {
+
+	static {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testConnection() {
+		try (Connection con = DriverManager.getConnection(
+				"jdbc:oracle:thin:@localhost:1521/XEPDB1", 
+				"mytest", 
+				"mytest"
+				)) {
+			log.info(con);
+			log.info("정상적으로 연결되었습니다.");
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	
+	
+	
+	
+}
